eacd26e05fe4c39948eec3bbdc94fd1445419829
A website for champions <feedweed.io@gmail.com>
2012-05-21 01:48:51 -0300

    My initial commit message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feedweed::Application.config.secret_token = 'e95b088d6a3973247c86e2268c44ae8b4753c3ab6c014b84acd309df487326ec775a23f0a2c5372536ca0ae5f3881c169deacac7e6989ba41bb85663a5344136'
